ecd242fa2b2d4534aa7c5fc135f1abc5f14bc327
Hariz <syedhariz1995@gmail.com>
2023-04-05 04:14:07 +0800

    added proper banner with dynamic infos

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**', // The path pattern for your image URLs
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
