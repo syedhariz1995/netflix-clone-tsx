6d7366e79b83753dabe00a6460edecf779fe9acb
Hariz <syedhariz1995@gmail.com>
2023-04-05 06:55:34 +0800

    added scroll and proper paddings in rows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import requests from '../../utils/requests'
 import { Movie } from '../../typings'
+import Row from '../../components/Row'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -40,14 +41,15 @@ const Home  = ({
       <Header/>
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals}/>
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+        <section className='md:space-y-24'>
+        <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component*/}         
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
