d7b57ae117b3c6290d04d0d19b44ce72ec20852c
Hariz <syedhariz1995@gmail.com>
2023-04-06 23:09:45 +0800

    again

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -13,7 +13,7 @@ function Thumbnail({movie}:Props) {
   const [showModal, setShowModal] = useRecoilState(modalState)
   const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
 
-  return ( 
+  return (
     <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 
     ease-out md:h-36 md:min-w-[260px] md:hover:scale-105" onClick={() => {
       setCurrentMovie(movie)
